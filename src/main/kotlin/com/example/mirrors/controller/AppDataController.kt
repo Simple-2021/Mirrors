69f400d11c6bb2fc9a33fc97fba62163e7f3ae58
Mojave <light.link.point@gmail.com>
2022-08-28 01:54:44 +0800

    Builder

--- src/main/kotlin/com/example/mirrors/controller/AppDataController.kt
+++ src/main/kotlin/com/example/mirrors/controller/AppDataController.kt
@@ -1,6 +1,5 @@
 package com.example.mirrors.controller
 
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
@@ -15,27 +14,32 @@ class AppDataController {
     private lateinit var mirrors: LinkedList<String>
 
     @PostMapping("/")
+    fun postMirror(mirror: String) {
+        if (mirror.isNotEmpty()) synchronized(this) { mirrors.add(mirror) }
+    }
+
+    @GetMapping("/api")
     fun getCount(): Int {
         return mirrors.size
     }
 
-    @DeleteMapping("/")
-    fun removeAll() {
-        for (i in 1 until mirrors.size) mirrors.removeLast()
-    }
-
-    @GetMapping("/api")
-    fun getMirror(): HashMap<String, String> {
-        return object : HashMap<String, String>() {
+    @GetMapping("/api/mirror")
+    fun getMirror(): HashMap<String, Any> {
+        return object : HashMap<String, Any>() {
             init {
                 put("mirror", mirrors.last)
+                put("count", mirrors.size)
             }
         }
     }
 
     @PostMapping("/api")
-    fun postMirror(mirror: String) {
-        if (mirror.isNotEmpty()) synchronized(this) { mirrors.add(mirror) }
+    fun download() {
+        synchronized(this) {
+            for (i in mirrors) {
+                println(i)
+            }
+        }
     }
 
 
